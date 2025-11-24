d6675cf2d2a13a197b0ee7007e9f8f06cdbc6710
KoDa94 <davidkoci@proton.me>
2025-11-24 17:26:10 +0100

    Add Product.cs, Category.cs and PseudoDb.cs

---
+++ EshopForFun/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿namespace EshopForFun.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; private set; }
+        public string UniqueCategoryString { get; private set; }
+        public string Name { get;  private set; }
+        public string Description { get; private set; }
+
+       
+        public Category(int categoryId, string uniqueCategoryString, string name, string description)
+        {
+            CategoryId = categoryId;
+            UniqueCategoryString = uniqueCategoryString;
+            Name = name;
+            Description = description;
+           
+        }
+    }
+}
---
+++ EshopForFun/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace EshopForFun.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string UniqueProductString { get;  set; }
+        public string Name { get;  set; }
+        public string Description { get; set; }
+        
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Product(int id, string uniqueProductString, string name, string description, decimal price, int categoryId)
+        {
+            Id = id;
+            UniqueProductString = uniqueProductString;
+            Name = name;
+            Description = description;
+            Price = price;
+            CategoryId = categoryId;
+        }
+    }
+}
